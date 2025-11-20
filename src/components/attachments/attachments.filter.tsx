b159284d22a181585a8d745b2a0d339e7fcf6e0b
CrashKey5 <CrashKey5@protonmail.com>
2025-11-20 09:13:18 +0000

    Further work on attachments

--- src/components/attachments/attachments.filter.tsx
+++ src/components/attachments/attachments.filter.tsx
@@ -1,3 +1,5 @@
+import { FilterIcon } from "lucide-react";
+import { useStore } from "@/store";
 import type { ResolvedDeck } from "@/store/lib/types";
 import type { Card } from "@/store/schemas/card.schema";
 import { Button } from "../ui/button";
@@ -9,12 +11,23 @@ type Props = {
 
 export function FilterAttachmentButton(props: Props) {
   const { card, resolvedDeck } = props;
+  const changeList = useStore((state) => state.changeList);
+
+  function AttachmentButton(attachment: any) {
+    /* this button should toggle between bare and no style, and enable or disable the filter, respectively. */
+
+    const onClick = () => {
+      changeList("player", window.location.href);
+    };
+
+    return (
+      <Button iconOnly variant="bare" size="sm" onClick={onClick}>
+        <FilterIcon />
+      </Button>
+    );
+  }
 
   return <AttachmentButton attachment={getAttachment(card, resolvedDeck)} />;
-}
-
-function AttachmentButton(attachment: any) {
-  return <Button>{attachment.attachment.icon}</Button>;
 }
 
 function getAttachment(card: Card, resolvedDeck: ResolvedDeck) {
